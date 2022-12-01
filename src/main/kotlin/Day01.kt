d326207c1a1ba2d0c4902eb99f04d1e4f47fd5c4
Aldo Malerba <aldo.malerba@it.clara.net>
2022-12-01 08:00:37 +0100

    removes index

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,4 +1,4 @@
-fun main() {
+class Day01 {
     fun part1(input: List<String>): Int {
         var max = 0
         var current = 0
@@ -22,7 +22,8 @@ fun main() {
         val maxs = mutableMapOf(1 to 0, 2 to 0, 3 to 0)
         var current = 0
         var min = maxs.minBy { it.value }
-        input.forEachIndexed { index, calorie ->
+
+        for (calorie in input) {
             if(calorie.isNotEmpty()){
                 current += calorie.toInt()
                 val max = if (current > min.value){
@@ -35,17 +36,19 @@ fun main() {
             }
         }
 
-        current = 0
 
         return  maxs.values.sum()
     }
+}
 
+fun main() {
+    val day = Day01()
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 24000)
-    check(part2(testInput) == 45000)
+    check(day.part1(testInput) == 24000)
+    check(day.part2(testInput) == 45000)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(day.part1(input))
+    println(day.part2(input))
 }
