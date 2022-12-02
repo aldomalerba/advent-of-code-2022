66b0e86ac8b44c2d523759c3c00b60c5866615ba
Aldo Malerba <aldo.malerba@it.clara.net>
2022-12-02 11:55:08 +0100

    extracts round score calculation in a function

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -6,42 +6,33 @@ class Day02: Parts{
     private val draws = mapOf('X' to 'A', 'Y' to 'B', 'Z' to 'C')
     private val scores = mapOf('A' to 1, 'B' to 2, 'C' to 3)
 
-    override fun part1(input: String): Int {
-        val plays = parsInput(input)
+    override fun part1(input: String) = rounds(input).sumOf { roundScorePart1(it) }
+    override fun part2(input: String) = rounds(input).sumOf { roundScoresPart2(it) }
 
-        val totScore = plays.sumOf {
-            val myResponse = draws[it.second]
-            val opponent = it.first
+    private fun roundScorePart1(it: Pair<Char, Char>): Int {
+        val myResponse = draws[it.second]
+        val opponent = it.first
 
-            val score = when {
-                opponent == myResponse -> 3
-                wins[opponent] != myResponse -> 6
-                else -> 0
-            }
-
-            score + scores[myResponse]!!
+        val score = when {
+            opponent == myResponse -> 3
+            wins[opponent] != myResponse -> 6
+            else -> 0
         }
 
-        return totScore
+        return score + scores[myResponse]!!
     }
-    override fun part2(input: String): Int {
-        val plays = parsInput(input)
 
-        val totScore = plays.sumOf {
-            val opponent = it.first
+    private fun roundScoresPart2(match: Pair<Char, Char>): Int {
+        val opponent = match.first
 
-            when (it.second) {
-                'X' -> scores[wins[opponent]]!!
-                'Y' -> 3 + scores[opponent]!!
-                else -> 6 + scores[wins.filterValues { it == opponent }.keys.first()]!!
-            }
-
+        return when (match.second) {
+            'X' -> scores[wins[opponent]]!!
+            'Y' -> 3 + scores[opponent]!!
+            else -> 6 + scores[wins.filterValues { it == opponent }.keys.first()]!!
         }
-
-        return totScore
     }
 
-    private fun parsInput(input: String) = input.split("\n").filter { it.isNotEmpty() }.map { it.first() to it.last() }
+    private fun rounds(input: String) = input.split("\n").filter { it.isNotEmpty() }.map { it.first() to it.last() }
 }
 
 fun main() {
